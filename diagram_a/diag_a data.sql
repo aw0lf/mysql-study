eddd22b252d66568c6bc3c3c48ceb374a0bc06dc
aw0lf <adrianna,tycner@yahoo.com>
2018-06-12 18:45:56 +0200

    diagram a

---
+++ diagram_a/diag_a data.sql
@@ -0,0 +1,27 @@
+--- insert data into guests-------------
+insert into guests (LoginID,Password,Fname,Lname,Email,Phone,Address) 
+values('1111','1111','Jan','Kwiatek','jan.kwiatek@o2.pl', '123-112-123','Kwiatowa 5 64-100 Warszawa');
+insert into guests (LoginID,Password,Fname,Lname,Address) 
+values('2222','2222','Ala','Kotek','Polna 5 64-100 Warszawa');
+insert into guests (LoginID,Password,Fname,Lname,Email,Phone,Address) 
+values('3333','fs33','Franek','Szpak','franek.szpak@o2.pl', '777-112-123','Kwiatowa 6 67-100 Opole');
+insert into guests (LoginID,Password,Fname,Lname,Email) 
+values('4444','jk44','Jan','Kowalski','jan.kowalski@o2.pl');
+
+---insert data into rooms-------------
+
+insert into rooms(Number,Level,Style,Viev,Beds,Smoking,LoginID)
+values(1,3,'basic','park',3,'YES',1111);
+insert into rooms(Number,Level,Style,Viev,Beds,Smoking,LoginID)
+values(2,3,'premium','sea',4,'NO',2222);
+insert into rooms(Number,Level,Style,Viev,Beds,Smoking,LoginID)
+values(3,2,'basic','lake',2,'NO',2222);
+
+----insert data into reservations------
+insert into reservations(ResNumber,CheckIn,CheckOut,LoginID)
+  values(11,'2018-06-01 10:00:00','2018-06-10 11:30:45',3333);
+  insert into reservations(ResNumber,CheckIn,LoginID,Number)
+  values(12,'2018-06-05 10:00:00',2222,2); 
+insert into reservations(ResNumber,CheckIn,CheckOut,LoginID)
+  values(13,'2018-04-09 10:00:00','2018-04-13 10:00:00',2222);
+
---
+++ diagram_a/diag_a.sql
@@ -0,0 +1,83 @@
+﻿/*
+Created: 11.06.2018
+Modified: 11.06.2018
+Model: diagram_a
+Database: MySQL 5.7
+*/
+
+
+-- Create tables section -------------------------------------------------
+
+-- Table Guests
+
+CREATE TABLE `Guests`
+(
+  `LoginID` Int NOT NULL,
+  `Password` Char(4),
+  `Fname` Varchar(30),
+  `Lname` Varchar(30),
+  `Email` Varchar(100),
+  `Phone` Char(11),
+  `Address` Varchar(50)
+)
+;
+
+ALTER TABLE `Guests` ADD PRIMARY KEY (`LoginID`)
+;
+
+-- Table Rooms
+
+CREATE TABLE `Rooms`
+(
+  `Number` Int NOT NULL,
+  `Level` Int,
+  `Style` Varchar(30),
+  `Viev` Varchar(30),
+  `Beds` Int,
+  `Smoking` Char(3),
+  `LoginID` Int NOT NULL
+)
+;
+
+CREATE INDEX `IX_Request` ON `Rooms` (`LoginID`)
+;
+
+ALTER TABLE `Rooms` ADD PRIMARY KEY (`Number`)
+;
+
+-- Table Reservations
+
+CREATE TABLE `Reservations`
+(
+  `ResNumber` Int NOT NULL,
+  `CheckIn` Datetime,
+  `CheckOut` Datetime,
+  `LoginID` Int NOT NULL,
+  `Number` Int
+)
+;
+
+CREATE INDEX `IX_Book` ON `Reservations` (`LoginID`)
+;
+
+CREATE INDEX `IX_Reservation` ON `Reservations` (`Number`)
+;
+
+ALTER TABLE `Reservations` ADD PRIMARY KEY (`ResNumber`)
+;
+
+-- Create foreign keys (relationships) section ------------------------------------------------- 
+
+
+ALTER TABLE `Rooms` ADD CONSTRAINT `Request` FOREIGN KEY (`LoginID`) REFERENCES `Guests` (`LoginID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+;
+
+
+ALTER TABLE `Reservations` ADD CONSTRAINT `Book` FOREIGN KEY (`LoginID`) REFERENCES `Guests` (`LoginID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+;
+
+
+ALTER TABLE `Reservations` ADD CONSTRAINT `Reservation` FOREIGN KEY (`Number`) REFERENCES `Rooms` (`Number`) ON DELETE RESTRICT ON UPDATE RESTRICT
+;
+
+
